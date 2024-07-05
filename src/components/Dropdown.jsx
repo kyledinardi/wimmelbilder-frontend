4fe9fcc29fd017817d0ec7cfbfbaf69f72589670
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-07-05 13:37:14 -0400

    Hook up backend for validation

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,22 +1,53 @@
 import PropTypes from 'prop-types';
 import styles from '../style/Dropdown.module.css';
 
-function Dropdown({ characters, inlineStyles }) {
+function Dropdown({ illustration, characters, inlineStyles, coordinates }) {
+  async function handleClick(e) {
+    const selectedCoords = {
+      x: parseInt(coordinates.split(' ')[0], 10),
+      y: parseInt(coordinates.split(' ')[1], 10),
+    };
+
+    const character = e.currentTarget.dataset.name;
+
+    const responseStream = await fetch('http://localhost:3000/characters', {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ selectedCoords, character, illustration }),
+    });
+
+    const response = await responseStream.json();
+
+    if (response.found) {
+      console.log(`You found ${character}`);
+    } else {
+      console.log(`${character} is somewhere else`);
+    }
+  }
+
   return (
     <div className={styles.dropdown} style={inlineStyles}>
       {characters.map((character) => (
-        <div className={styles.character} key={character.name}>
+        <button
+          onClick={(e) => handleClick(e)}
+          className={styles.character}
+          data-name={character.name}
+          key={character.name}
+        >
           <img src={character.img} alt='' />
           <p>{character.name}</p>
-        </div>
+        </button>
       ))}
     </div>
   );
 }
 
 Dropdown.propTypes = {
+  illustration: PropTypes.string,
   characters: PropTypes.array,
   inlineStyles: PropTypes.object,
+  coordinates: PropTypes.string,
 };
 
 export default Dropdown;
